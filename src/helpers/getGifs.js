913adefc70ae1f7122ada975cbc21b864da60a86
luisanthony196 <luis.anthony.chavarria@gmail.com>
2022-05-01 14:32:45 -0500

    Correcion en url segura

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${process.env.REACT_APP_API_KEY}`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${process.env.REACT_APP_API_KEY}`
   const resp = await fetch(url)
   const {data} = await resp.json()
   const gifs = data.map(img => {
